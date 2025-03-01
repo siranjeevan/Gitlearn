fcd4b0176ad0fade6bc2f77632edcce1554c23d8
siranjeevan <smartjeevith2k@gamil.com>
2025-03-01 13:03:09 +0530

    Initial Commit

---
+++ Gitlearn/GitlearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitlearnApp.swift
+//  Gitlearn
+//
+//  Created by Jeevith  on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GitlearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
